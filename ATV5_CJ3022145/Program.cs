07f366fdc7268a61bccac3cf1077d7c053d39f43
Pedro Henrique de Sá Ferreira <f.sa@aluno.ifsp.edu.br>
2024-03-27 11:03:48 -0300

    Adicionar arquivos de projeto.

---
+++ ATV5_CJ3022145/Program.cs
@@ -0,0 +1,14 @@
+﻿int ativ; 
+Console.WriteLine("Digite o número da atividade que deseja: ");
+ativ = int.Parse(Console.ReadLine());
+
+switch(ativ)
+{
+
+
+    //Atividade 1
+case 1:
+        int n1, n2;
+        Console.WriteLine("Digite um número inteiro: ");
+        n1 = int.Parse(Console.ReadLine());
+        for (n1=1; n1!=n2)
